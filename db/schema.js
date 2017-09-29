6e72156293251dd46199f63577e48f30ac346fb8
raisking <raiprakas007@gmail.com>
2017-09-29 14:38:26 -0400

    update

--- db/schema.js
+++ db/schema.js
@@ -4,14 +4,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    name:{
-        type: String,
-        required: true
-    },
-    phone:{
-        type: Number,
-        required: true
-    }
     // email:{
     //     type: String,
     //     required: true
