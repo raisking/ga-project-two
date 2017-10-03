7a73326e1aa07ff63706fd8de2c3316d00371f3d
raisking <raiprakas007@gmail.com>
2017-10-02 20:46:19 -0400

    Add start

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,20 @@
 function myFunction() {
     document.body.style.backgroundColor = "red";
 }
- 
+
+// alert('hi');
+// $(document).ready(function(){
+//    $('#circle').click(function(){
+//     document.getElementById('appCover')
+//    }) 
+// })
+
+function myFunction() {
+    var x = document.getElementById("appCover");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } 
+    else {
+        x.style.display = "none";
+    }
+}
